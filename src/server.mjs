e0241abce5f6cbbbfdc133fd51da7fd3e93581e2
Waliangga <waliangga@gmail.com>
2022-07-18 19:05:13 +0700

    [FIX] bugs host value

--- src/server.mjs
+++ src/server.mjs
@@ -4,10 +4,10 @@ import { routes } from "./routes.mjs";
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
-                origin: ['http://notesapp-v1.dicodingacademy.com/'],
+                origin: ['*'],
             },
             
         }
