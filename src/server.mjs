1e9fcf6a457eab34bab21069f3b3da59db30b888
Waliangga <waliangga@gmail.com>
2022-07-18 12:36:29 +0700

    init commit

---
+++ src/server.mjs
@@ -0,0 +1,22 @@
+import Hapi from "@hapi/hapi";
+import { routes } from "./routes.mjs";
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['http://notesapp-v1.dicodingacademy.com/'],
+            },
+            
+        }
+    });
+
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
